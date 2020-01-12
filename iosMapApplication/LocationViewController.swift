e02399ede2653d3f4a121e33fb1afedc8c94251d
alisengur <alisengur96@gmail.com>
2020-01-12 22:41:27 +0300

    new editing commit

---
+++ iosMapApplication/LocationViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  iosMapApplication
+//
+//  Created by Ali Şengür on 6.01.2020.
+//  Copyright © 2020 Ali Şengür. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class LocationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+
+
+    var locationNameArray = [String]()
+    var latitudeArray = [Double]()
+    var longitudeArray = [Double]()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(createButtonClicked))
+        
+        getData()
+    }
+    
+    
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
+    
+
+    
+    @objc func getData() {
+        
+        locationNameArray.removeAll(keepingCapacity: false)
+        latitudeArray.removeAll(keepingCapacity: false)
+        longitudeArray.removeAll(keepingCapacity: false)
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Location")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {   // gets data from Location entity
+                if let addressName = result.value(forKey: "locationName") as? String {
+                    self.locationNameArray.append(addressName)   
+                }
+                
+                if let latitude = result.value(forKey: "latitude") as? Double {
+                    self.latitudeArray.append(latitude)
+                }
+                
+                if let longitude = result.value(forKey: "longitude") as? Double {
+                    self.longitudeArray.append(longitude)
+                }
+                
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("error")
+        }
+    }
+    
+
+    
+    
+    
+    @objc func createButtonClicked() {
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as? MapViewController {
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationNameArray.count
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewItem", for: indexPath)
+        if !locationNameArray[indexPath.row].isEmpty {
+            cell.textLabel?.text = "\(locationNameArray[indexPath.row])"
+        } else {
+            cell.textLabel?.text = ""
+        }
+        return cell
+    }
+    
+    
+
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let coord = CLLocationCoordinate2D(latitude: latitudeArray[indexPath.row], longitude: longitudeArray[indexPath.row])  // gets coordinates from selected row.
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as? MapViewController {
+            vc.location = coord
+            self.navigationController?.pushViewController(vc, animated: true)   // sends coordinates to MapViewController.
+        }
+        
+    }
+    
+}
+
--- iosMapApplication/MapViewController.swift
+++ iosMapApplication/MapViewController.swift
@@ -15,13 +15,24 @@ import CoreLocation
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var addressTF: UITextField!
-    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    @IBAction func changeMapType(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            mapView.mapType = .standard
+        } else {
+            mapView.mapType = .satellite
+        }
+    }
+    
+    
+    @IBOutlet weak var confirmButton: UIButton!
+    
     
     var location: CLLocationCoordinate2D?
     var willAddNewAnnotation: Bool = false
-    var selectedCoordinates: CLLocationCoordinate2D?
-    // lazy var geocoder = CLGeocoder()
+    var selectedLocation: UITextField?
     
     
     
@@ -29,115 +40,66 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapWillAppear()
-        addressTF.isUserInteractionEnabled = false
-//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
-//        view.addGestureRecognizer(gestureRecognizer)
-    }
-    
-    
-    
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotationOnLongPress(gesture:)))
-        longPressGesture.minimumPressDuration = 1.0
-        self.mapView.addGestureRecognizer(longPressGesture)
-    }
-    
-    
-    
-    
-    
-    
-    
-    @objc func addAnnotationOnLongPress(gesture: UILongPressGestureRecognizer) {
-        if gesture.state == .ended {
-            let point = gesture.location(in: self.mapView)
-            let coordinate = self.mapView.convert(point, toCoordinateFrom: self.mapView)
-            selectedCoordinates = coordinate
-            print(coordinate)
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = coordinate
-//            annotation.title = "Title"
-//            annotation.subtitle = "subtitle"
-            self.mapView.addAnnotation(annotation)
-            addressTF.isUserInteractionEnabled = true
-        }
-    }
-    
-    
-    
-    
-    
-    func mapWillAppear() {
         self.mapView.delegate = self
-        guard let cord = self.location else { return }
-        let pin = MKPointAnnotation()
-        pin.coordinate = cord
-        self.mapView.addAnnotation(pin)
-        addressTF.isHidden = true
-        saveButton.isHidden = true
+        if location != nil {
+            confirmButton.isHidden = true
+        }
+        
     }
     
     
-
-
+    
     
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         return nil
     }
-
     
     
-//    func mapWillAppear() {
-//        if willAddNewAnnotation == true {
-//            self.mapView.delegate = self
-//            guard let cord = self.location else { return }
-//            let pin = MKPointAnnotation()
-//            pin.coordinate = cord
-//            self.mapView.addAnnotation(pin)
-//        } else {
-//            self.mapView.delegate = self
-//        }
-//    }
-    
-
     
     
-    @IBAction func saveButtonClicked(_ sender: UIButton) {
-
-        guard let coord = self.selectedCoordinates else { return }
-        print(coord.latitude)
-        print(coord.longitude)
+    @IBAction func confirmButtonClicked(_ sender: UIButton) {
+        let coordinate = self.mapView.convert(self.view.center, toCoordinateFrom: self.mapView)
+        print(coordinate)
+        let alert = UIAlertController(title: "Are you sure to save?", message: nil, preferredStyle: UIAlertController.Style.alert)   // creating the alert
+        alert.addTextField(configurationHandler: { (textField:UITextField)->Void  in    // inserting text field to alertview.
+                    textField.placeholder = "enter the title of the location"
+                })       //Add text field
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertAction.Style.default) { (_) in
+            if let titleText = alert.textFields?[0].text {
+                self.saveLocationToCoreData(location: titleText, latitude: coordinate.latitude, longitude: coordinate.longitude)    // getting the text and sending to saveLocationWithCoreData function.
+            }
+            
+        }
+        alert.addAction(saveAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))   // adding the actions
+        self.present(alert, animated: true, completion: nil)    // showing the alert
         
+    }
+    
+    
+    
+    
+    func saveLocationToCoreData(location: String, latitude: Double, longitude: Double) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        let newAddress = NSEntityDescription.insertNewObject(forEntityName: "Coordinate", into: context)
-        
-        if let addressName = addressTF.text, addressName.count > 0 {
-            newAddress.setValue(addressName, forKey: "addressName")
-            newAddress.setValue(coord.latitude, forKey: "latitude")
-            newAddress.setValue(coord.longitude, forKey: "longitude")
-            
-            do {
-                try context.save()
-                print("successful")
-            } catch {
-                print("failed")
-            }
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "newData"), object: nil)
-            addressTF.text?.removeAll()
-            addressTF.isUserInteractionEnabled = false
-           // self.navigationController?.popViewController(animated: true)
-        } else {
-            let alert = UIAlertController(title: "Error", message: "Title cannot be blank.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Ok", style: .default))
-            self.present(alert, animated: true, completion: nil)
+        let newLocation = NSEntityDescription.insertNewObject(forEntityName: "Location", into: context)
+        newLocation.setValue(location, forKey: "locationName")
+        newLocation.setValue(latitude, forKey: "latitude")
+        newLocation.setValue(longitude, forKey: "longitude")
+        do {
+            try context.save()  // saving the name, latitude and longitude
+            print("successful")
+        } catch {
+            print("failed")
         }
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
+    
+    
+    
+    
     
     
     
@@ -152,91 +114,14 @@ class MapViewController: UIViewController {
 
 extension MapViewController: MKMapViewDelegate {
 
-    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
-        if let center = location {
-            let region = MKCoordinateRegion(center: center, latitudinalMeters: 400, longitudinalMeters: 400)
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {  // it works when map loaded.
+        if let center = location {  // if location variable exist, setRegion works.
+            let regionRadius: CLLocationDistance = 1000.0
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: regionRadius , longitudinalMeters: regionRadius)
             self.mapView.setRegion(region, animated: true)
+            self.mapView.showsUserLocation = true
+            
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    func getAddress() {
-//
-//        let geocoder: CLGeocoder = CLGeocoder()
-//        if let selectedLocation = selectedLocation {
-//            geocoder.reverseGeocodeLocation(selectedLocation, completionHandler:
-//            {(placemarks, error) in
-//                if (error != nil)
-//                {
-//                    print("reverse geodcode fail: \(error!.localizedDescription)")
-//                }
-//                let pm = placemarks! as [CLPlacemark]
-//
-//                if pm.count > 0 {
-//                    let pm = placemarks![0]
-//                    var addressString : String = ""
-//                    if pm.subLocality != nil {
-//                        addressString = addressString + pm.subLocality! + ", "
-//                    }
-//                    if pm.thoroughfare != nil {
-//                        addressString = addressString + pm.thoroughfare! + ", "
-//                    }
-//                    if pm.locality != nil {
-//                        addressString = addressString + pm.locality! + ", "
-//                    }
-//                    if pm.country != nil {
-//                        addressString = addressString + pm.country! + ", "
-//                    }
-//
-//                    if let subLocality = pm.subLocality,
-//                        let thoroughfare = pm.thoroughfare,
-//                        let locality = pm.locality,
-//                        let country = pm.country
-//                    {
-//                        self.saveAdress(subLocality: subLocality, thoroughFare: thoroughfare, locality: locality, country: country)
-//                    }
-//                    print(addressString)
-//                }
-//            })
-//        }
-//    }
-    
-    
-
-//    func saveAdress(subLocality: String, thoroughFare: String, locality: String, country: String ) {
-//
-//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//        let context = appDelegate.persistentContainer.viewContext
-//
-//        let newAddress = NSEntityDescription.insertNewObject(forEntityName: "Address", into: context)
-//        newAddress.setValue(subLocality, forKey: "subLocality")
-//        newAddress.setValue(thoroughFare, forKey: "thoroughFare")
-//        newAddress.setValue(locality, forKey: "locality")
-//        newAddress.setValue(country, forKey: "country")
-//
-//        do {
-//            try context.save()
-//            print("successful")
-//        } catch {
-//            print("failed")
-//        }
-//
-//
-//    }
